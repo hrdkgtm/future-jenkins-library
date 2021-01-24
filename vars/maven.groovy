9cc32323c9cd7ac881c8632574c20c5f6dec6afb
Hardika Gutama <hardika.gutama@gdn-commerce.com>
2021-01-24 22:37:15 +0700

    add library

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -23,7 +23,19 @@ def call(Map param){
             stage('Deliver') {
                 steps {
                     sh './jenkins/scripts/deliver.sh'
+                    // Load script from library with package path
+                    def my_script = libraryResource 'scripts/deliver.sh'
+
+                    // create a file with script_bash content
+                    writeFile file: './deliver.sh', text: my_script
+
+                    // Run it!
+                    sh './deliver.sh'
+
+                    // Don't forget to clean up your mess
+                    sh 'rm -rf ./deliver.sh'
                 }
+
             }
         }
     }
